e61286913deaba807bf86c0734e06543891d2896
matangeorgi <matang@wix.com>
2023-02-19 18:39:42 +0200

    new

---
+++ src/components/inline-style/inline-style.tsx
@@ -0,0 +1,21 @@
+import styles from './inline-style.module.scss';
+
+export interface InlineStyleProps {
+    className?: string;
+}
+
+export const InlineStyle = ({ className }: InlineStyleProps) => {
+    return (
+        <div
+            className={`${styles.root} ${className}`}
+            style={{
+                background: 'red',
+                fontStyle: 'italic',
+                position: 'relative',
+                boxShadow: '2.83px 2.83px 2px 51px black',
+            }}
+        >
+            InlineStyle
+        </div>
+    );
+};
