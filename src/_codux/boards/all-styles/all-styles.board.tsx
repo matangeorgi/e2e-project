1cadc6ea399f277161f81832b9527e9c59a112de
matan georgi <matang@wix.com>
2024-05-15 11:07:21 +0300

    sad

--- src/_codux/boards/all-styles/all-styles.board.tsx
+++ src/_codux/boards/all-styles/all-styles.board.tsx
@@ -2,6 +2,6 @@ import { createBoard } from '@wixc3/react-board';
 import { AllStyles } from '../../../components/all-styles/all-styles';
 
 export default createBoard({
-    name: 'AllStyleasdaskjkjjkdasds',
+    name: 'AllStyleasdaskjkjjkdasdasdsds',
     Board: () => <AllStyles />,
 });
